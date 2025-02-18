15a3ee7ad8884e7301bca3890aec7425b028da66
DaniVera-tech <daniss.vera@gmail.com>
2025-02-18 19:18:10 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import "bootstrap";
-import "./style.css";
+window.onload = function () {
+  
+    // Arrays con las opciones
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
+    // Seleccionar elementos al azar
+    let randomWho = Math.floor(Math.random() * who.length);
+    let randomAction = Math.floor(Math.random() * action.length);
+    let randomWhat = Math.floor(Math.random() * what.length);
+    let randomWhen = Math.floor(Math.random() * when.length);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    // Concatenar la excusa
+    let excuse = `${who[randomWho]} ${action[randomAction]} ${what[randomWhat]} ${when[randomWhen]}`;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    // Mostrar la excusa en el elemento con id "excuse"
+    document.getElementById("excuse").innerText = excuse;
+  }
+
+  
